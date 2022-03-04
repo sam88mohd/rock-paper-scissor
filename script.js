33b5642d7294c6fe13eb3194109811f0d608f52a
samsudin mohamad <sam88mohd@gmail.com>
2022-03-05 01:33:52 +0800

    working function

--- script.js
+++ script.js
@@ -1,38 +1,72 @@
+// choice array
+const array = ["rock", "paper", "scissor"];
+
+// variables
+let round = 1;
+let playerScore = 0;
+let computerScore = 0;
+let drawScore = 0;
+
 // function to generate random selection from array
 function computerPlay() {
-  const array = ["rock", "paper", "scissor"];
-  return array[Math.floor(Math.random() * 3)];
+  return Math.floor(Math.random() * 3);
 }
 
 // function to play single round
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "rock" && computerSelection === "paper") {
-    return console.log("User Loose!");
-  }
-  if (playerSelection === "paper" && computerSelection === "paper") {
-    return console.log("Draw!");
-  }
-  if (playerSelection === "scissor" && computerSelection === "paper") {
-    return console.log("User Win!");
-  }
-  if (playerSelection === "rock" && computerSelection === "rock") {
-    return console.log("Draw!");
-  }
-  if (playerSelection === "paper" && computerSelection === "rock") {
-    return console.log("User Win!");
-  }
-  if (playerSelection === "scissor" && computerSelection === "rock") {
-    return console.log("User Loose!");
-  }
-  if (playerSelection === "rock" && computerSelection === "scissor") {
-    return console.log("User Win!");
-  }
-  if (playerSelection === "paper" && computerSelection === "scissor") {
-    return console.log("User Loose!");
-  }
-  if (playerSelection === "scissor" && computerSelection === "scissor") {
-    return console.log("Draw!");
+  if (array.indexOf(playerSelection) === 0 && computerSelection === 2) {
+    return "User Win!";
+  } else if (array.indexOf(playerSelection) === 2 && computerSelection === 0) {
+    return "Computer Win!";
+  } else if (array.indexOf(playerSelection) > computerSelection) {
+    return "User Win!";
+  } else if (array.indexOf(playerSelection) === computerSelection) {
+    return "Draw";
+  } else {
+    return "Computer Win!";
   }
 }
 
-function game() {}
+// show who win
+function whoWin(playerScore, computerScore) {
+  if (playerScore > computerScore) {
+    return alert("You win");
+  } else {
+    return alert("You loose");
+  }
+}
+
+// query element
+const btnContainer = document.querySelector(".button-div");
+const resultContainer = document.querySelector("#result");
+const playerScoreElement = document.querySelector("#player-score span");
+const computerScoreElement = document.querySelector("#computer-score span");
+const roundElement = document.querySelector("#round span");
+
+// attached event listener to button
+btnContainer.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    if (round < 5) {
+      const playerSelection = e.target.dataset.value;
+      const computerSelection = computerPlay();
+      const result = playRound(playerSelection, computerSelection);
+      resultContainer.textContent = result;
+      switch (result) {
+        case "User Win!":
+          playerScore++;
+          break;
+        case "Computer Win!":
+          computerScore++;
+          break;
+        default:
+          drawScore++;
+      }
+      round++;
+      playerScoreElement.textContent = playerScore;
+      computerScoreElement.textContent = computerScore;
+      roundElement.textContent = round;
+    } else {
+      whoWin(playerScore, computerScore);
+    }
+  }
+});
